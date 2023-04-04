a9a1290b6c914990a6aac91f331880986b6e2c0a
Job Henandez Lara <hj93@protonmail.com>
2023-04-04 12:51:49 -0700

    Update download_pdfs.py

--- download_pdfs.py
+++ download_pdfs.py
@@ -12,13 +12,15 @@ args = parser.parse_args()
 
 def downloads_pdfs(url, folder):
     
-    #If there is no such folder, the script will create one automatically
     if not os.path.exists(folder):os.mkdir(folder)
+        
     response = requests.get(url)
+    
     soup = BeautifulSoup(response.text, "html.parser")
+    
     for link in soup.select("a[href$='.pdf']"):
-        #Name the pdf files using the last portion of each link which are unique in this case
         filename = os.path.join(folder, link['href'].split('/')[-1])
+        
         with open(filename, 'wb') as f:
             f.write(requests.get(urljoin(url,link['href'])).content)
 
